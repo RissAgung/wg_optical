e0b815e0c821e8eb6f471cb8f5f18536c9c67607
mphstar <bintang.simdig20@gmail.com>
2022-12-04 17:30:28 +0700

    Validasi input master pegawai & Multilevel & Fix Session Timeout

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -5,31 +5,44 @@ session_start();
 $kon = new Koneksi();
 
 if (isset($_POST['type'])) {
-    if($_POST['type'] == 'login'){
+    if ($_POST['type'] == 'login') {
 
         $email = $_POST['txt_email'];
         $password = $_POST['txt_password'];
-    
+
         $query = "SELECT * FROM pegawai WHERE email = '$email'";
         $result = $kon->execute($query);
         $num = mysqli_num_rows($result);
-    
+
         while ($row = mysqli_fetch_array($result)) {
             $namaval = $row['nama'];
             $emailval = $row['email'];
             $passwordval = $row['password'];
+            $idlevelval = $row['id_level'];
         }
-    
+
         if ($num != 0) {
             if ($emailval == $email && $passwordval == md5($password)) {
-                $response = array(
-                    'status' => 'success',
-                    'msg' => 'Login Berhasil'
-                );
-                echo json_encode($response);
-                $_SESSION["statusLogin"] = "true";
-                $_SESSION["email"] = $namaval;
-                exit();
+
+                if ($idlevelval == 3) {
+                    $response = array(
+                        'status' => 'success_roles',
+                        'msg' => 'Anda Login Sebagai Sales'
+                    );
+                    echo json_encode($response);
+                    exit();
+                } else {
+
+                    $response = array(
+                        'status' => 'success',
+                        'msg' => 'Login Berhasil'
+                    );
+                    echo json_encode($response);
+                    $_SESSION["statusLogin"] = "true";
+                    $_SESSION["email"] = $namaval;
+                    $_SESSION['level'] = $idlevelval;
+                    exit();
+                }
             } else {
                 $response = array(
                     'status' => 'error',
@@ -45,9 +58,8 @@ if (isset($_POST['type'])) {
             );
             echo json_encode($response);
             exit();
-    
         }
-    } else if($_POST['type'] == 'logout'){
+    } else if ($_POST['type'] == 'logout') {
         session_destroy();
     }
 }
