eabdc3f8ec5cf672355596caf4569fa15ed43110
mphstar <bintang.simdig20@gmail.com>
2022-12-17 17:43:51 +0700

    Fix query transaksi

--- controllers/transaksiController.php
+++ controllers/transaksiController.php
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 // }
 
                 // move to frame transaksi
-                $crud->execute("INSERT INTO frame_transaksi (kode_detail_pesanan, harga) SELECT kode_pesanan, harga FROM keranjang_frame WHERE kode_pesanan = '" . $data[$i]["kode"] . "'");
+                $crud->execute("INSERT INTO frame_transaksi (kode_detail_pesanan, harga, kode_frame) SELECT keranjang_frame.kode_pesanan, keranjang_frame.harga, detail_bawa.Kode_Frame FROM keranjang_frame JOIN detail_bawa ON keranjang_frame.id_bawa = detail_bawa.Id_Bawa JOIN produk ON detail_bawa.Kode_Frame = produk.kode_frame WHERE kode_pesanan = '" . $data[$i]["kode"] . "'");
 
                 // move to lensa transaksi
                 $crud->execute("INSERT INTO lensa_transaksi (kode_detail_pesanan, kode_varian_lensa_transaksi, id_jenis_lensa, harga) SELECT kode_pesanan, kode_varian_lensa_keranjang, id_jenis_lensa, harga FROM keranjang_lensa WHERE kode_pesanan = '" . $data[$i]["kode"] . "'");
@@ -42,6 +42,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                 // move detail varian lensa
                 $crud->execute("INSERT INTO detail_varian_lensa_transaksi (kode_varian_lensa_transaksi, kode_lensa) SELECT detail_varian_lensa_keranjang.kode_varian_lensa_keranjang, detail_varian_lensa_keranjang.kode_lensa FROM detail_varian_lensa_keranjang JOIN keranjang_lensa ON detail_varian_lensa_keranjang.kode_varian_lensa_keranjang = keranjang_lensa.kode_varian_lensa_keranjang WHERE keranjang_lensa.kode_pesanan = '" . $data[$i]["kode"] . "'");
+
+                // delete keranjang frame to trigger detail bawa
+                $crud->execute("DELETE FROM keranjang_frame WHERE kode_pesanan = '" . $data[$i]["kode"] . "'");
+
+                // hapus keranjang
+                $crud->execute("DELETE FROM keranjang WHERE kode_pesanan = '" . $data[$i]["kode"] . "'");
+
             }
         }
     }
