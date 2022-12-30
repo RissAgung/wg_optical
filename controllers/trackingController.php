5a5a207c08f943d84268e904d5243d5f67a06716
mphstar <bintang.simdig20@gmail.com>
2022-12-30 14:15:11 +0700

    add fitur tracking & finishing

---
+++ controllers/trackingController.php
@@ -0,0 +1,21 @@
+<?php
+include '../config/koneksi.php';
+$crud = new koneksi();
+
+if(isset($_POST['kode_pesanan'])){
+
+    $res = $crud->showData("SELECT customer.nama, transaksi.kode_pesanan, transaksi.status_pengiriman FROM transaksi JOIN customer ON transaksi.id_customer = customer.id_customer WHERE status_confirm = 2 AND status_pengiriman IS NOT NULL AND transaksi.kode_pesanan = '".$_POST['kode_pesanan']."'");
+    foreach ($res as $value) {
+        $json[] = array(
+            'nama'=> $value['nama'],
+            'kode_pesanan'=> $value['kode_pesanan'],
+            'status_pengiriman'=> $value['status_pengiriman'],
+        );
+    }
+
+    echo json_encode($json);
+
+
+}
+
+?>
